daf4336d9cf3764dc32858eca4936087ad15b041
riddhi <riddhi.kumari@indusos.com>
2022-01-11 16:02:20 +0530

    first commit

---
+++ src/lib/shared/SEO.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	export let title: string = 'Svelte Kit App Bazaar';
+	export let description: string = 'Devloper Dashboard';
+</script>
+
+<svelte:head>
+	<title>{title}</title>
+	<meta name="description" content={description} />
+	<html lang="en" />
+</svelte:head>
